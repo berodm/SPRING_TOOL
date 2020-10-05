a53ae3f54395233c72b71855ad887638918b0d41
Beatriz Rodrigues <70703527+berodm@users.noreply.github.com>
2020-10-05 18:22:10 -0300

    Minha Escola - Alunos

---
+++ alunos/src/main/java/com/minhaEscola/alunos/model/Alunos.java
@@ -0,0 +1,48 @@
+package com.minhaEscola.alunos.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Alunos")
+public class Alunos {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private boolean matriculado;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public boolean isMatriculado() {
+		return matriculado;
+	}
+
+	public void setMatriculado(boolean matriculado) {
+		this.matriculado = matriculado;
+	}
+
+}
